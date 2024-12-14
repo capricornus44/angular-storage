d00c61d62af164e7af5fc1706e6718a05f3a8f05
capricornus44 <antonzavalniuk@gmail.com>
2024-12-14 14:38:57 +0100

    add auth forms validation

--- src/app/shared/auth-form/auth-form.component.ts
+++ src/app/shared/auth-form/auth-form.component.ts
@@ -1,18 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { FORM } from '../../types/form.type';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'auth-form',
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.css',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
 })
 export class AuthFormComponent implements OnInit {
-  type: FORM = 'login';
+  authForm!: FormGroup;
+  isLoginMode: boolean = true;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    this.type = window.location.pathname.split('/')[1] as FORM;
+    this.isLoginMode = this.router.url === '/login';
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      ...(this.isLoginMode
+        ? {}
+        : {
+            username: ['', [Validators.required, Validators.minLength(3)]],
+          }),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.authForm.invalid) {
+      this.authForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = this.authForm.value;
+    if (this.isLoginMode) {
+      console.log('Login data:', formData);
+      // Handle login logic here
+    } else {
+      console.log('Register data:', formData);
+      // Handle register logic here
+    }
   }
 }
